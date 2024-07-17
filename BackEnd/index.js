b02b33acf425ecd5b6084b03ae18e03a09eb2a44
EyaSomaii <eya.somai@esprit.tn>
2024-07-17 16:19:28 +0200

    com

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+
 const app = express();
 const port = 3000;
+// Connect To base mongodb 
+mongoose.connect('mongodb+srv://eyasomai:0000@chatapp.kekwsyw.mongodb.net/chatApp', {
+  
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+// End Connect To base mongodb 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
